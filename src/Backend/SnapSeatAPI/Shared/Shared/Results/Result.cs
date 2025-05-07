971097bd35fa91be02381b1ccb34baf7e0c88c98
mohamadhesham00 <mohamadhesham112@gmail.com>
2025-05-07 23:57:58 +0300

    Initial Create for shared class library

---
+++ src/Backend/SnapSeatAPI/Shared/Shared/Results/Result.cs
@@ -0,0 +1,34 @@
+﻿namespace Shared.Results
+{
+    using System.Net;
+
+    public class Result<T>
+    {
+        public bool IsSuccess { get; }
+        public T? Value { get; }
+        public string? Error { get; }
+        public HttpStatusCode StatusCode { get; }
+
+        private Result(T value, HttpStatusCode statusCode)
+        {
+            IsSuccess = true;
+            Value = value;
+            StatusCode = statusCode;
+        }
+
+        private Result(string error, HttpStatusCode statusCode)
+        {
+            IsSuccess = false;
+            Error = error;
+            StatusCode = statusCode;
+        }
+
+        public static Result<T> Success(T value) =>
+            new(value, HttpStatusCode.OK);
+
+        public static Result<T> Failure(string error, HttpStatusCode statusCode = HttpStatusCode.BadRequest) =>
+            new(error, statusCode);
+    }
+
+
+}
