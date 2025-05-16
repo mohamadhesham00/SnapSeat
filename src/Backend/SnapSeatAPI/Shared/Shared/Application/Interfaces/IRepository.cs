b8866a7e11f96f19c7ddc8a8929ed4d2a27e90fc
mohamadhesham00 <mohamadhesham112@gmail.com>
2025-05-16 22:36:02 +0300

    FEAT: added shared Repository interface and implementation that has basic implementation for CRUD operations

---
+++ src/Backend/SnapSeatAPI/Shared/Shared/Application/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Shared.Application.Interfaces
+{
+    public interface IRepository<TEntity>
+    {
+        Task<TEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+        IQueryable<TEntity> GetAllAsync();
+        Task AddAsync(TEntity entity, CancellationToken cancellationToken);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        Task<bool> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ src/Backend/SnapSeatAPI/Shared/Shared/Infrastructure/Repository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Application.Interfaces;
+
+namespace Shared.Infrastructure
+{
+    public class Repository<TEntity, TDbContext> : IRepository<TEntity>
+        where TEntity : class
+        where TDbContext : DbContext
+    {
+        protected readonly TDbContext _db;
+        protected readonly DbSet<TEntity> _dbSet;
+
+        public Repository(TDbContext db)
+        {
+            _db = db;
+            _dbSet = db.Set<TEntity>();
+        }
+
+        public virtual async Task<TEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            return await _dbSet.FindAsync(id, cancellationToken);
+        }
+
+        public virtual IQueryable<TEntity> GetAllAsync()
+        {
+            return _dbSet;
+        }
+
+        public async Task AddAsync(TEntity entity, CancellationToken cancellationToken)
+        {
+            await _dbSet.AddAsync(entity, cancellationToken);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public async Task<bool> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            return await _db.SaveChangesAsync(cancellationToken) > 0;
+        }
+
+    }
+}
