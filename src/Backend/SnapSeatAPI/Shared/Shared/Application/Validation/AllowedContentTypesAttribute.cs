51ac01cc5ba8eebdb79fc3fd008b23ab25d70580
mohamadhesham00 <mohamadhesham112@gmail.com>
2025-05-16 22:52:28 +0300

    FEAT: added service for uploading image and store it in file management system & attribute for validating the image input

---
+++ src/Backend/SnapSeatAPI/Shared/Shared/Application/Validation/AllowedContentTypesAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Application.Validation
+{
+    public class AllowedContentTypesAttribute : ValidationAttribute
+    {
+        private readonly string[] _allowedTypes;
+
+        public AllowedContentTypesAttribute(params string[] allowedTypes)
+        {
+            _allowedTypes = allowedTypes;
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is IFormFile file)
+            {
+                if (!_allowedTypes.Contains(file.ContentType))
+                {
+                    return new ValidationResult($"File type {file.ContentType} is not allowed.");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+
+}
