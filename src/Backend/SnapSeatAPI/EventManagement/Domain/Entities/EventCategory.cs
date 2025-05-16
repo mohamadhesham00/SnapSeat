58259f7c8a44b305318849aff0a1814f4e5a5f66
mohamadhesham00 <mohamadhesham112@gmail.com>
2025-05-16 22:43:10 +0300

    FEAT: added full implementation for event category ( entity - service - repository - mapping)

---
+++ src/Backend/SnapSeatAPI/EventManagement/Domain/Entities/EventCategory.cs
@@ -0,0 +1,16 @@
+﻿using Shared.Domain.Models;
+
+namespace EventManagement.Domain.Entities
+{
+    public class EventCategory : BaseEntity
+    {
+        public string Name { get; set; } = null!;
+
+        //for EfCore
+        private EventCategory() { }
+        public EventCategory(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ src/Backend/SnapSeatAPI/EventManagement/Infrastructure/Persistence/Repositories/EventCategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using EventManagement.Application.Interfaces.IRepos;
+using EventManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Shared.Infrastructure;
+
+namespace EventManagement.Infrastructure.Persistence.Repositories
+{
+    public class EventCategoryRepository : Repository<EventCategory, EventDBContext>, IEventCategoryRepository
+    {
+
+        public EventCategoryRepository(EventDBContext db) : base(db) { }
+
+        public async Task<bool> Exists(Guid id)
+        {
+            return await _dbSet.AnyAsync(ec => ec.Id == id);
+        }
+
+    }
+}
