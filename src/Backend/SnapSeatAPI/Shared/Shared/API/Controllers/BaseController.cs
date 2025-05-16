c4458b263f5c8a3fda3d985f6f1238356a2cba24
mohamadhesham00 <mohamadhesham112@gmail.com>
2025-05-16 22:17:38 +0300

    FEAT: Added resuable base controller and an extension method to handle result

---
+++ src/Backend/SnapSeatAPI/Shared/Shared/API/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared.Domain.Models;
+
+namespace Shared.API.Controllers
+{
+    public abstract class BaseController : ControllerBase
+    {
+        protected CurrentUser CurrentUser => HttpContext.Items["CurrentUser"] as CurrentUser;
+    }
+}
---
+++ src/Backend/SnapSeatAPI/Shared/Shared/API/Controllers/ControllerExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared.Domain.Models;
+
+namespace Shared.API.Controllers
+{
+    public static class ControllerExtensions
+    {
+        public static IActionResult HandleResult<T>(this ControllerBase controller, Result<T> result)
+        {
+            if (!result.IsSuccess)
+            {
+                return controller.StatusCode((int)result.StatusCode, new { error = result.Error });
+            }
+
+            return controller.StatusCode((int)result.StatusCode, result.Value);
+        }
+    }
+}
