6c90b0195763d0cac439ba84bd7096706100f76d
mohamadhesham00 <mohamadhesham112@gmail.com>
2025-05-17 08:24:21 +0300

    BUILD: Initial create for Booking entity & migration for adding it to db

---
+++ src/Backend/SnapSeatAPI/Modules/BookingAPI/DependencyInjection/BookingModule.cs
@@ -0,0 +1,20 @@
+﻿using BookingAPI.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BookingAPI.DependencyInjection
+{
+    public static class BookingModule
+    {
+        public static IServiceCollection AddBookingModule(this IServiceCollection services,
+        IConfiguration configuration)
+        {
+            services.AddDbContext<BookingDBContext>(opt =>
+                opt.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+
+            return services;
+        }
+    }
+}
---
+++ src/Backend/SnapSeatAPI/Modules/BookingAPI/Domain/Entities/Booking.cs
@@ -0,0 +1,11 @@
+﻿using Shared.Domain.Models;
+
+namespace BookingAPI.Domain.Entities
+{
+    public class Booking : BaseEntity
+    {
+        public Guid TicketTypeId { get; set; }
+        public DateTime BookingTime { get; set; }
+        public int Seats { get; set; }
+    }
+}
